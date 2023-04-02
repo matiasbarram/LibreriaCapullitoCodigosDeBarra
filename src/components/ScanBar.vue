dcc5c17b15638d6d4589ff2fab948f179cf2bce6
Matias Barra <matias.barra.ap@copec.cl>
2023-04-01 22:35:42 -0300

    Add weird code to sel cam

--- src/components/ScanBar.vue
+++ src/components/ScanBar.vue
@@ -11,9 +11,55 @@ import Quagga from '@ericblade/quagga2'; // ES6
 const emit = defineEmits(['emitData'])
 const loading = ref(true)
 
+const doCamThings = async () => {
+  /* get user's permission to muck around with video devices */
+  const tempStream = await navigator.mediaDevices.getUserMedia({ video: true })
+  const devices = await navigator.mediaDevices.enumerateDevices()
+  let frontDeviceId
+  let backDeviceId
+  if (devices.length > 0) {
+    /* defaults so all this will work on a desktop */
+    frontDeviceId = devices[0].deviceId;
+    backDeviceId = devices[0].deviceId;
+  }
+  /* look for front and back devices */
+  devices.forEach(device => {
+    if (device.kind === 'videoinput') {
+      if (device.label && device.label.length > 0) {
+        if (device.label.toLowerCase().indexOf('back') >= 0)
+          backDeviceId = device.deviceId
+        else if (device.label.toLowerCase().indexOf('front') >= 0)
+          frontDeviceId = device.deviceId
+      }
+    }
+  })
+  /* close the temp stream */
+  const tracks = tempStream.getTracks()
+  if (tracks)
+    for (let t = 0; t < tracks.length; t++) tracks[t].stop()
+  /* open the device you want */
+  const constraints = {
+    video: true,
+    deviceId: { exact: backDeviceId }
+  }
+
+  return navigator.mediaDevices.getUserMedia(constraints)
+}
+
 onMounted(() => {
   start()
   detecting()
+  if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) {
+    navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } })
+      .then(function (stream) {
+        var video = document.querySelector("#videoWindow");
+        video.srcObject = doCamThings();
+      })
+      .catch(function (error) {
+        console.error("Error al acceder a la cámara: " + error);
+      });
+  }
+
 })
 
 
