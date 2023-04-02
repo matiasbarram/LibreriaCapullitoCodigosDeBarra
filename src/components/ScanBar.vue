f140b613424caf4a74c707c1f90e6957c0dc3e8c
Matias Barra <matias.barra.ap@copec.cl>
2023-04-02 12:59:32 -0400

    Add cam selector

--- src/components/ScanBar.vue
+++ src/components/ScanBar.vue
@@ -1,6 +1,10 @@
 <template>
   <Loader v-if="loading" />
-  <div v-show="!loading" id="videoWindow" class="video"></div>
+  <div v-show="!loading">
+    <div id="videoWindow" class="video"></div>
+    <select name="input-stream_constraints" id="deviceSelection">
+    </select>
+  </div>
 </template>
 
 <script setup>
@@ -18,25 +22,49 @@ const constraints = {
 };
 
 onMounted(() => {
+  initCameraSelection()
   start()
   detecting()
 
 })
 
+// inputStream: {
+//       name: "live",
+//       type: "LiveStream",
+//       target: document.querySelector("#videoWindow"),
+//       constraints: {
+//         width: 640,
+//         height: 1281,
+//         facingMode: "environment",
+
+//       },
+//     },
 
 const start = async () => {
   const config = {
     locate: true,
     inputStream: {
-      name: "live",
       type: "LiveStream",
       target: document.querySelector("#videoWindow"),
-      constraints: {
-        width: 640,
-        height: 1281,
-        facingMode: "environment",
 
-      },
+      constraints: {
+        width: { min: 640 },
+        height: { min: 480 },
+        facingMode: "environment",
+        aspectRatio: { min: 1, max: 2 }
+      }
+    },
+    locator: {
+      patchSize: "medium",
+      halfSample: true
+    },
+    numOfWorkers: 2,
+    frequency: 10,
+    decoder: {
+      readers: [{
+        format: "code_128_reader",
+        config: {}
+      }]
     },
     decoder: {
       readers: ["ean_reader"],
@@ -58,11 +86,6 @@ const start = async () => {
     Quagga.start();
   });
 
-  const stream = await navigator.mediaDevices.getUserMedia(constraints)
-  console.log(stream)
-  const video = document.querySelector('video');
-  video.srcObject = stream;
-  video.play();
 }
 
 const detecting = () => {
@@ -74,7 +97,28 @@ const detecting = () => {
     console.log("Found: " + barcode)
     emit("emitData", barcode);
   });
+}
 
+const initCameraSelection = async () => {
+  var streamLabel = Quagga.CameraAccess.getActiveStreamLabel();
+
+  return Quagga.CameraAccess.enumerateVideoDevices()
+    .then(function (devices) {
+      function pruneText(text) {
+        return text.length > 30 ? text.substr(0, 30) : text;
+      }
+      var $deviceSelection = document.getElementById("deviceSelection");
+      while ($deviceSelection.firstChild) {
+        $deviceSelection.removeChild($deviceSelection.firstChild);
+      }
+      devices.forEach(function (device) {
+        var $option = document.createElement("option");
+        $option.value = device.deviceId || device.id;
+        $option.appendChild(document.createTextNode(pruneText(device.label || device.deviceId || device.id)));
+        $option.selected = streamLabel === device.label;
+        $deviceSelection.appendChild($option);
+      });
+    });
 }
 
 </script>
@@ -82,5 +126,15 @@ const detecting = () => {
 <style>
 canvas.drawingBuffer {
   display: none !important;
+}
+
+select {
+  padding: 8px;
+  font-size: 16px;
+  border-radius: 4px;
+  border: 1px solid #ccc;
+  background-color: #fff;
+  color: #333;
+  margin: 20px 0;
 }
 </style>
