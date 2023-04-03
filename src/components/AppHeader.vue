772e02053968fe98233e75ce32fe75938afa9772
Matias Barra <matias.barra.ap@copec.cl>
2023-04-02 20:41:59 -0400

    fix open and close navbar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -16,10 +16,9 @@
     </div>
   </header>
 
-  <nav class="menu" :class="{ open: isMenuOpen }">
+  <nav class="menu" :class="{ open: isMenuOpen }" ref="target">
     <div class="main-menu">
-      <button class="hamburger-menu menu-toggle" aria-expanded="false" aria-label="Toggle menu" @click="toggleMenu"
-        ref="target">
+      <button class="hamburger-menu menu-toggle" aria-expanded="false" aria-label="Toggle menu" @click="toggleMenu">
         <svg viewBox="0 0 24 24" width="24" height="24" class="hamburger-icon">
           <path fill="currentColor" d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
         </svg>
