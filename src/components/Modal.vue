e6f995a3fc2111caca348acde3c6f869340ed567
Matias Barra <matias.barra.ap@copec.cl>
2023-04-08 16:47:09 -0400

    delete clickout to close the modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="modal">
-        <div class="modal-content" ref="target">
+        <div class="modal-content">
             <div class="close-action" @click="closeModal">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="close-icon">
                     <path fill="#000" fill-rule="evenodd"
@@ -38,9 +38,6 @@ const props = defineProps({
 onMounted(() => {
     fetchDataFromApi()
 })
-
-onClickOutside(target, (event) => closeModal())
-
 
 const fetchDataFromApi = async () => {
     try {
