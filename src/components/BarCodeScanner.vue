0adf5bb5ade82ef7c61ee9889e2b7266acc24910
Matias Barra <matias.barra.ap@copec.cl>
2023-04-24 16:56:38 -0400

    Revert "Revert "Add Pancho constraints""
    This reverts commit d744b4c8869c225ddf286a47a878ad93ec23fd47.

--- src/components/BarCodeScanner.vue
+++ src/components/BarCodeScanner.vue
@@ -5,7 +5,7 @@
       <Alert message="¿Problemas al escanear?" subMessage="Intenta con otra cámara." />
     </div>
     <div id="videoWindow" class="video"></div>
-    <div v-show="isAndroid" style="margin:10px 0">
+    <div style="margin:10px 0">
       <div class="options-container">
         <div v-show="hasTorchCap">
           <button class="torch" @click="onChangeTorchTest()">
@@ -22,7 +22,7 @@
           <button class="zoom" @click="onChangeZoomTest('down')" :disabled="actualZoomValue <= zoomValue.min">-</button>
           <button class="zoom" @click="onChangeZoomTest('up')" :disabled="actualZoomValue >= zoomValue.max">+</button>
         </div>
-        <div class="select-container">
+        <div v-show="isAndroid" class="select-container">
           <select v-model="selectedCamera" @change="onChange()">
             <option v-for="option in options" :value="option.value">{{ option.label }}</option>
           </select>
@@ -68,9 +68,7 @@ const checkAndroidCamera20 = async () => {
     selectedCamera.value = camera20.deviceId
     localStorage.setItem("deviceId", camera20.deviceId)
     await Quagga.stop();
-    start({
-      deviceId: camera20.deviceId
-    })
+    start()
   }
 }
 
@@ -122,9 +120,7 @@ const changeCapabilities = async (props) => {
 
 
 onMounted(() => {
-  start({
-    deviceId: localStorage.getItem("deviceId")
-  })
+  start()
 })
 
 
@@ -135,11 +131,13 @@ const start = (constraints) => {
       type: "LiveStream",
       target: document.querySelector("#videoWindow"),
       constraints: {
+        width: '1920',
+        height: '1080',
         aspectRatio: {
           ideal: 1,
         },
-        ...constraints
-      }
+        focusMode: 'continuous',
+      },
     },
     locator: {
       patchSize: "large",
@@ -163,15 +161,15 @@ const start = (constraints) => {
     detecting()
     checkZoomCapability()
     checkTorchCapability()
-    if (selectedCamera.value == "") {
-      if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/Windows/i)) {
-        isAndroid.value = true
-        checkAndroidCamera20().then(() => {
-          addSelectOptions()
-          selectDefaultCamera()
-        })
-      }
-    }
+    // if (selectedCamera.value == "") {
+    //   if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/Windows/i)) {
+    //     isAndroid.value = true
+    //     checkAndroidCamera20().then(() => {
+    //       addSelectOptions()
+    //       selectDefaultCamera()
+    //     })
+    //   }
+    // }
   })
 }
 
